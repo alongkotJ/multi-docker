bbc03c82da15d37f472e584976b543b4cef67e75
AlongkotJ <alongkot.sw@gmail.com>
2019-12-17 00:44:09 +0700

    test re-deploy

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          <p>Edit <code>src/App.js</code> and save to reload. ==> V.2</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
